c6cc9d2b8c906b01ab9add7fce734bf887437537
Evan Sosenko <evan.sosenko@meltwater.com>
2018-10-05 13:52:58 -0700

    Add got API

--- examples/get.js
+++ examples/get.js
@@ -1,7 +1,7 @@
 import { createHttpClient } from '../lib'
 
 export default ({ log, baseUrl }) => async (url = '/get') => {
-  const client = createHttpClient({ baseUrl })
+  const client = createHttpClient({ baseUrl, log })
   const { body } = await client.get(url)
   return body
 }
