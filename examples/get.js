9fb27c106efc487f368ad42abc5ed18b3fe14be7
Evan Sosenko <evan.sosenko@meltwater.com>
2018-10-05 11:42:45 -0700

    Add factory

--- examples/get.js
+++ examples/get.js
@@ -1,7 +1,7 @@
-import { HttpClient } from '../lib'
+import { createHttpClient } from '../lib'
 
 export default ({ log, baseUrl }) => async (url = '/get') => {
-  const client = new HttpClient({ baseUrl })
+  const client = createHttpClient({ baseUrl })
   const { body } = await client.get(url)
   return body
 }
