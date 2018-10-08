28f0d807bb35aac563217737b7dff81c66505e37
Evan Sosenko <evan.sosenko@meltwater.com>
2018-10-08 10:22:49 -0700

    Update examples to return

--- examples/get.js
+++ examples/get.js
@@ -2,8 +2,7 @@ import { createClient } from '../lib'
 
 export default ({ log, baseUrl }) => async (url = '/get') => {
   const client = createClient({ baseUrl, log })
-  const { body } = await client.get(url, {
+  return client.get(url, {
     responseLogLevel: 'info'
   })
-  return body
 }
--- examples/post.js
+++ examples/post.js
@@ -2,7 +2,7 @@ import { createClient } from '../lib'
 
 export default ({ log, baseUrl }) => async (url = '/post') => {
   const client = createClient({ baseUrl, log })
-  await client.post(url, {
+  return client.post(url, {
     body: { foo: 'bar' },
     logProps: { fooId: 123 },
     meta: { fooName: 'fooman' },
